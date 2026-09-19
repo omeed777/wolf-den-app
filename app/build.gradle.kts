d6abe4915f4e04f47bcb7900f9e38181b36c6cd0
omeed777 <126956952+omeed777@users.noreply.github.com>
2026-09-19 09:18:02 +0330

    Add ViewModel Compose support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.activity:activity-compose:1.10.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
